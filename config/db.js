11b1e6b787ad4d34987644baec24ca38c49385ae
yvesgdlmx <gerencia.tecnologias@optimex.mx>
2024-11-11 17:07:25 -0600

    n

--- config/db.js
+++ config/db.js
@@ -6,7 +6,6 @@ dotenv.config({path: '.env'})
 const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS ?? '', {
     host: process.env.BD_HOST,
     port: process.env.DB_PORT || 3306,
-    port: 3306,
     dialect: 'mysql',
     timezone: 'America/Mexico_City',
     define: {
--- routes/ReportesProduccionRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { obtenerReportesProduccion } from '../controllers/ReportesController.js';
-
-const router = express.Router();
-
-router.get('/reportes/produccion', obtenerReportesProduccion)
-
-export default router;
