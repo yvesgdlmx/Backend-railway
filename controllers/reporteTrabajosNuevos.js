5f13d914ed3b87a5d35757829bd8db826cc8cd92
yvesgdlmx <gerencia.tecnologias@optimex.mx>
2024-12-06 13:54:42 -0600

    3

--- controllers/reporteTrabajosNuevos.js
+++ controllers/reporteTrabajosNuevos.js
@@ -1,11 +1,12 @@
 import TrabajosNuevos from '../models/TrabajosNuevos.js';
-import moment from 'moment-timezone';
+import moment from 'moment';
 
 const obtenerDatosTrabajosNuevos = async (req, res) => {
     try {
         // Encuentra la fecha más reciente en la base de datos
         const fechaMasReciente = await TrabajosNuevos.max('fecha');
-        console.log("Fecha más reciente encontrada:", fechaMasReciente);
+        console.log("Fecha más reciente encontrada en bruto:", fechaMasReciente);
+        
         if (!fechaMasReciente) {
             return res.status(404).json({
                 msg: "No se encontraron registros de trabajos nuevos",
@@ -14,20 +15,20 @@ const obtenerDatosTrabajosNuevos = async (req, res) => {
                 }
             });
         }
-        
-        // Ajusta la zona horaria al formatear la fecha
-        const fechaFormateada = moment(fechaMasReciente).tz('America/Mexico_City').format('YYYY-MM-DD');
+
+        // Especifica el formato de entrada para la fecha
+        const fechaFormateada = moment(fechaMasReciente, 'YYYY-MM-DD').format('YYYY-MM-DD');
         console.log("Fecha formateada para la consulta:", fechaFormateada);
-        
+
         // Obtén todos los registros con la fecha más reciente
         const registros = await TrabajosNuevos.findAll({
             where: {
-                fecha: fechaFormateada // Asegúrate de que solo compare la fecha
+                fecha: fechaFormateada
             },
-            order: [['hora', 'ASC']], // Ordena por hora de manera ascendente
+            order: [['hora', 'ASC']],
             raw: true
         });
-        
+
         res.json({
             total: registros.length,
             registros: registros
