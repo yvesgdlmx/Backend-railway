2660c41ba85229428c011cf908a27b48b15fa8e2
yvesgdlmx <gerencia.tecnologias@optimex.mx>
2026-01-12 17:44:01 -0600

    ajustes

--- controllers/wipTotalController.js
+++ controllers/wipTotalController.js
@@ -85,8 +85,8 @@ const obtenerWipTotalPorRango = async (req, res) => {
     const { anioInicio, mesInicio, diaInicio, anioFin, mesFin, diaFin } = req.params;
     try {
         // Crear fechas de inicio y fin en formato 'YYYY-MM-DD'
-        const fechaInicio = moment.tz(`${anioInicio}-${mesInicio}-${diaInicio}`, 'America/Mexico_City').startOf('day').toDate();
-        const fechaFin = moment.tz(`${anioFin}-${mesFin}-${diaFin}`, 'America/Mexico_City').endOf('day').toDate();
+        const fechaInicio = moment(`${anioInicio}-${mesInicio}-${diaInicio}`).startOf('day').toDate();
+        const fechaFin = moment(`${anioFin}-${mesFin}-${diaFin}`).endOf('day').toDate();
 
         // Buscar registros en el rango
         const registros = await WipTotal.findAll({
